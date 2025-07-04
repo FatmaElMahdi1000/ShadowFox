91d0090383237a5a5df4ace8343967edd96d183c
FatmaElMahdi1000 <f69246984@gmail.com>
2025-07-05 02:39:51 +0300

    ShadowFox internship - Task1-Beginner

---
+++ TaskLevelBeginner/.ipynb_checkpoints/Variables-checkpoint.py
@@ -0,0 +1,17 @@
+print("---#Task #1: 1---")
+pi = 22/7   # 22/7 ~ 3.14 ratio of a circle's circumference to its diameter.
+print(type(pi)) #checking the data type of the variables.
+
+
+print("---#Task #1: 2---")
+for = 4
+print(for) 
+#for is already a reserved keyword for python, thus, we'll end up with invalid syntax here.
+
+
+print("---#Task #1: 3---")
+Principal_Amount = 10000
+Interest_Rate = 0.1
+Duration =  3  
+Simple_Interest = Principal_Amount * Interest_Rate  * Duration
+print(Simple_Interest)
---
+++ TaskLevelBeginner/Numbers.py
@@ -0,0 +1,7 @@
+def numbers(num, formatting):
+    return format(num, formatting)
+
+number = 145
+formatting = 'o'  #octal representation of a number 
+Final = numbers(number, formatting)
+print(Final)
