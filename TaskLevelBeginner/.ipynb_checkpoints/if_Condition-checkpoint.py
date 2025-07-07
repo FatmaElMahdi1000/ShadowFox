9c55b386705ed75864a6cb2cfd79c57f57c24aa3
FatmaElMahdi1000 <f69246984@gmail.com>
2025-07-08 01:37:10 +0300

    ShadowFox internship!

--- TaskLevelBeginner/.ipynb_checkpoints/if_Condition-checkpoint.py
+++ TaskLevelBeginner/.ipynb_checkpoints/if_Condition-checkpoint.py
@@ -1,5 +1,4 @@
-
-print("##____Body Mass Index______##")
+print("##1.____Body Mass Index______##")
 
 Height = float(input("Your Height in m: "))
 Weight = int(input("Your Weight in Kg: "))
@@ -17,4 +16,36 @@ elif BMI > 18.5 and BMI < 25:
     print("Normal")
 else:
     print("Underweight")
+
+
+print("##2.____To determine which country a city belongs to.______##")
+
+Australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+UAE = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+India = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+
+
+City_name = input("Enter a city name: ")
+
+if City_name in Australia:
+    print(f"{City_name} is in Australia")
+
+elif City_name in UAE:
+    print(f"{City_name} is in UAE")
+else:
+    print(f"{City_name} is in India")
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
