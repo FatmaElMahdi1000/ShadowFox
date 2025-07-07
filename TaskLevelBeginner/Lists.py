405f7ba5eb48be9a7b50a23978a2c8b8296a47ed
FatmaElMahdi1000 <f69246984@gmail.com>
2025-07-07 22:53:33 +0300

    ShadowFox Internship!

---
+++ TaskLevelBeginner/Lists.py
@@ -0,0 +1,41 @@
+justice_league = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman", "Green Lantern"]
+
+justice_league_members = len(justice_league)
+print(f"1. justice_league_members = {justice_league_members} member!") #Task1
+
+justice_league.extend(["Batgirl", "Nightwing"])#New_members_added
+print(f"2. updated list of members: {justice_league} ") #Task2
+
+
+for i, value in enumerate(justice_league):
+    if justice_league[i] == "Wonder Woman":
+        justice_league[i] = justice_league[0]
+        justice_league[0] = "Wonder Woman"
+print(f"3. updated list of members: {justice_league}")  #Task3
+
+for i, value in enumerate(justice_league):
+    if justice_league[i] == "Aquaman" and justice_league[i + 1] ==  "Green Lantern":
+        justice_league[i + 1]= justice_league[i] 
+        justice_league[i] = "Green Lantern"
+        
+print(f"4. updated list of members: {justice_league}") #Task4
+
+print("#___Another way to do no.4___USING: Remove(), Index(), insert() methods#")
+justice_league= ['Wonder Woman', 'Batman', 'Superman', 'Flash',   'Aquaman', 'Green Lantern', 'Batgirl', 'Nightwing']
+justice_league.remove("Green Lantern")
+Flash_index = justice_league.index('Flash')
+Placing_Green_Lantern = justice_league.insert(Flash_index + 1, "Green Lantern")
+print(justice_league)
+
+
+del justice_league[0:]
+justice_league.extend(["Cyborg", "Shazam", "Hawkgirl", "Martian Manhunter", "Green Arrow"])
+print(f"5. New list of members: {justice_league}")  #Task5
+
+
+justice_league.sort()
+print(f"6. justice_league_sorted_alphabetically: {justice_league}")
+
+new_leader = justice_league[0]
+
+print(f"BOUNS QUESTION: NEW LEADER IS: {new_leader}")  #Task6
--- TaskLevelBeginner/Numbers.py
+++ TaskLevelBeginner/Numbers.py
@@ -1,3 +1,4 @@
+print("---Numbers 1---")
 def numbers(num, formatting):
     return format(num, formatting)
 
@@ -5,3 +6,35 @@ number = 145
 formatting = 'o'  #octal representation of a number 
 Final = numbers(number, formatting)
 print(Final)
+
+
+print("---Numbers 2---")
+
+def Pond_Area(radius):
+    pi = 3.14
+    Area = int(pi * (radius**2)) #square meters (area is in square unit)
+    water_total_amount = int(Area * 1.4)
+    return Area, water_total_amount
+
+radius = 84
+Result = Pond_Area(radius) # m²
+print(Result) 
+
+print("---Numbers 3---")
+Distance = 490 #meterlong
+Time = 7 #min 
+Time_in_sec = 7 * 60 
+speed = int(Distance/Time_in_sec) #distance/time 
+print(f"Speed is going to be {speed}")
+
+
+
+
+
+
+
+
+
+
+
+
